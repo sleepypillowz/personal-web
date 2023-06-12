c24ed4af8bcf447a78ebcedcd85866e63b204e01
sleepypillowz <alexacojido@gmail.com>
2023-06-12 23:22:05 +0800

    changed project to support offline

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling on anchor links
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollLinks = document.querySelectorAll('a[href^="#"]');
+    for (const scrollLink of scrollLinks) {
+      scrollLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = scrollLink.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      });
+    }
+  });
+  

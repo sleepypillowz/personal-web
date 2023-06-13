5ab3ade6533cedd33896dc64cf668629f74913cf
sleepypillowz <alexacojido@gmail.com>
2023-06-13 16:40:22 +0800

    added colors to skills

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Smooth Scrolling
 document.addEventListener('DOMContentLoaded', function() {
     var navLinks = document.querySelectorAll('.nav-link');
     for (var i = 0; i < navLinks.length; i++) {
@@ -12,3 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }
   });
+
+  // Change Skill Color
+  document.addEventListener("DOMContentLoaded", function() {
+    const skillProgressBars = document.querySelectorAll(".skill-progress-bar");
+  
+    skillProgressBars.forEach(function(progressBar) {
+      const width = parseInt(progressBar.style.width, 10);
+  
+      if (width >= 0 && width <= 24) {
+        progressBar.style.backgroundColor = "#FA8072";
+      } else if (width >= 25 && width <= 74) {
+        progressBar.style.backgroundColor = "#F0E68C";
+      } else if (width >= 75 && width <= 100) {
+        progressBar.style.backgroundColor = "#50C878";
+      }
+    });
+  });
+  

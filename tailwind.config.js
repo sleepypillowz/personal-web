8f346ff50d27abdb7aa1f7ec00dbab864f6aeafc
Alexander Acojido <118715035+sleepypillowz@users.noreply.github.com>
2024-08-21 22:39:36 +0800

    website overhaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: ["./src/index.html"],
   theme: {
     extend: {
-      backgroundImage: {
-        'polka': "url('/assets/images/polka.png')"
-      },
-      fontFamily: {
-        ubuntu: ['Ubuntu']
+      colors: {
+        'light-orange': '#FFD18E',
+        'dark-blue': '#7FA1C3',
+        'light-blue': '#BBE9FF',
+        'blue': '#92B4EC',
+        'light-white': '#FAF9F6',
+        'light-gray': '#E5E4E2',
       }
     },
   },

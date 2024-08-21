143c58be8caa851b677464579f4380f8e67bcf19
Alexander Acojido <118715035+sleepypillowz@users.noreply.github.com>
2024-08-22 05:30:00 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/index.html"],
+  darkMode: 'selector',
   theme: {
-    extend: {
-      colors: {
-        'light-orange': '#FFD18E',
-        'dark-blue': '#7FA1C3',
-        'light-blue': '#BBE9FF',
-        'blue': '#92B4EC',
-        'light-white': '#FAF9F6',
-        'light-gray': '#E5E4E2',
-      }
-    },
+    fontFamily: {
+      'inter': ['Inter'],
+    }
   },
-  plugins: [],
 }
 
